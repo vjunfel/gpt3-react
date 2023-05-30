c8f047215a2b0b5b95ddcf4a6dc6d40ee382d1e6
vjunfel <vjunfel@gmail.com>
2023-05-30 15:33:18 +0800

    GPT-3 project on react

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Brand, CTA, Navbar } from './components';
+import {
+	Footer,
+	Blog,
+	Possibility,
+	Features,
+	WhatGPT3,
+	Header,
+} from './containers';
+import './app.css';
+
+function App() {
+	return (
+		<div className="App">
+			<div className="gradient__bg">
+				<Navbar />
+				<Header />
+			</div>
+			<Brand />
+			<WhatGPT3 />
+			<Features />
+			<Possibility />
+			<CTA />
+			<Blog />
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
