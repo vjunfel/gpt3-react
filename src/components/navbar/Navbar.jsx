a6c53b7e6cb82d100e47b17a7700015e9dc74ba4
vjunfel <vjunfel@gmail.com>
2023-06-04 07:55:33 +0800

    Added figma file and fix the link of possibilty

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,8 +9,8 @@ const Menu = () => (
 	<>
 		<p><a href="#home">Home</a></p>
 		<p><a href="#wgpt3">What is GPT?</a></p>
-		<p><a href="#possibility">Open AI</a></p>
 		<p><a href="#features">Case Studies</a></p>
+		<p><a href="#possibility">Open AI</a></p>
 		<p><a href="#blog">Library</a></p>
 		
 	</>
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -4,7 +4,7 @@ import './possibility.css';
 
 const Possibility = () => {
 	return (
-		<div className="gpt3__possibility section__padding">
+		<div className="gpt3__possibility section__padding" id='possibility'>
 			<div className="gpt3__possibility-image">
 				<img src={possibilityImage} alt="posibility" />
 			</div>
