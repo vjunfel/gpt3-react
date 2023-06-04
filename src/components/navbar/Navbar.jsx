bfccce5ac3ec8b02879779e99efab5993969c837
vjunfel <vjunfel@gmail.com>
2023-06-04 09:16:16 +0800

    added link to a logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -22,7 +22,9 @@ const Navbar = () => {
 		<div className="gpt3__navbar">
 			<div className="gpt3__navbar-links">
 				<div className="gpt3__navbar-links_logo">
-					<img src={logo}	alt="logo"/>
+					<a href="https://gpt3.junfelv.com">
+						<img src={logo}	alt="logo"/>
+					</a>
 				</div>
 				<div className="gpt3__navbar-links_container">
 					<Menu />
